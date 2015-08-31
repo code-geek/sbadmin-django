fbdbed871093a5c8bebc8a8015b220148f7f9b2c
Ashish Acharya <ashish.acharya14@gmail.com>
2015-09-01 00:45:48 +0545

    changed home url to login to avoid sidebar javascript issue

--- adminbase/dashboard/urls.py
+++ adminbase/dashboard/urls.py
@@ -2,14 +2,14 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name="index"),
+    url(r'^$', views.LoginView.as_view(), name="login"),
+    url(r'^index/$', views.IndexView.as_view(), name="index"),
     url(r'^blank/$', views.BlankView.as_view(), name="blank"),
     url(r'^buttons/$', views.ButtonsView.as_view(), name="buttons"),
     url(r'^flot/$', views.FlotView.as_view(), name="flot"),
     url(r'^forms/$', views.FormsView.as_view(), name="forms"),
     url(r'^grid/$', views.GridView.as_view(), name="grid"),
     url(r'^icons/$', views.IconsView.as_view(), name="icons"),
-    url(r'^login/$', views.LoginView.as_view(), name="login"),
     url(r'^morris/$', views.MorrisView.as_view(), name="morris"),
     url(r'^notifications/$', views.NotificationsView.as_view(), name="notifications"),
     url(r'^panels/$', views.PanelsView.as_view(), name="panels"),
