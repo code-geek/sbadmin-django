82d9fca871a0a48615708a7100441f38a8163acb
Ashish Acharya <ashish.acharya14@gmail.com>
2015-08-31 22:13:24 +0545

    added namespace for dashboard

--- adminbase/adminbase/urls.py
+++ adminbase/adminbase/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include('dashboard.urls')),
+    url(r'^', include('dashboard.urls', namespace='dashboard')),
     url(r'^admin/', include(admin.site.urls)),
 ]
